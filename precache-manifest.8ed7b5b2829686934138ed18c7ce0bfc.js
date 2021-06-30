19815ca1f593107195553d03de5f8ae896d117d4
Code Sandbox <hello@codesandbox.io>
2021-06-30 17:18:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8ed7b5b2829686934138ed18c7ce0bfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd3516158a72789fc8a791e8d41b85e",
+    "url": "https://davidvromano.github.io/csb-reysc/index.html"
+  },
+  {
+    "revision": "3cb9467fceb93d2602cf",
+    "url": "https://davidvromano.github.io/csb-reysc/static/js/2.a7d772a9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://davidvromano.github.io/csb-reysc/static/js/2.a7d772a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ca2ab507984f8185b4",
+    "url": "https://davidvromano.github.io/csb-reysc/static/js/main.5d56ff63.chunk.js"
+  },
+  {
+    "revision": "68bc68e31e66afdb7ff8",
+    "url": "https://davidvromano.github.io/csb-reysc/static/js/runtime-main.db8b98df.js"
+  }
+]);
